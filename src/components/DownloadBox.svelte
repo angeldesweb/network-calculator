7942d02cbf1049222dcd0939b17c4aedfefca829
angeldesweb <angeldesweb@gmail.com>
2021-10-19 04:09:38 -0400

    Adding downloads Links

--- src/components/DownloadBox.svelte
+++ src/components/DownloadBox.svelte
@@ -1,15 +1,15 @@
 <script>
 	import { lang } from './../store.js';
-
 </script>
 
 <div class="down-box">
-    <button>
+    <a class="button" href="https://www.dropbox.com/s/somgctv9hrsc7jq/network-calculator.msi?dl=1" alt="Windows app">
         {$lang === 'en-US' ? 'Download for Windows' : 'Descarga para Windows'}
-    </button>
-    <button>
-        {$lang === 'en-US' ? 'Download for Linux' : 'Descargar para Linux'}
-    </button>
+    </a>
+    <br>
+    <a class="button" href="https://www.dropbox.com/s/arve4d7i8oxquo3/network-calculator-linux-x64.rar?dl=1" alt="Linux app">
+       {$lang === 'en-US' ? 'Download for Linux' : 'Descargar para Linux'}
+    </a>
 </div>
 
 <style>
@@ -17,13 +17,17 @@
         display: flex;
         flex-direction: column;
     }
-    .down-box > button {
+    .down-box > .button {
         margin-top:1em;
         background-color: var(--main-blue);
         color:#fff;
+        text-align: center;
+        height:3em;
+        line-height: 3em;
     }
     
-    .down-box > button:hover {
+    .down-box > .button:hover {
+        text-decoration: none;
         background-color: var(--third-blue);
     }
 </style>
